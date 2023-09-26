8de451b8d3a092eb94d3ee9e7310727890c15c46
AfireHong <varhong2018@gmail.com>
2023-09-26 18:50:17 +0800

    chore: init

---
+++ src/components/demo.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const emits = defineEmits<{
+  (e: "do-something"): void;
+}>();
+</script>
+<template>
+  <div>
+    <button @click="$emit('do-something')">demo</button>
+  </div>
+</template>
+<style lang="stylus" scoped></style>
